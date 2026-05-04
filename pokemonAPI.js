15c26aa6be04046210c19350444ce689a4083279
Galimellow <alexpanes92@gmail.com>
2026-05-05 00:25:29 +0300

    Add files via upload

---
+++ pokemonAPI.js
@@ -0,0 +1,33 @@
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Could not fetch data");
+        }
+        return response.json()
+    })
+    .then(data => console.log(data.name))
+    .catch(error => console.log(error));
+
+async function fetchData(){
+
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        
+        if(!response.ok){
+            throw new Error("Could not fetch data");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonImg");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+        imgElement.style.width = "25%";
+    }
+    catch(error){
+        console.log(error);
+    }
+}
