87895c5c83ad7b7453a8aa3595b9723b571c1dba
Galimellow <alexpanes92@gmail.com>
2026-05-05 23:51:46 +0300

    Refactor pokemon API fetching and display logic

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -40,3 +40,44 @@ async function fetchData(){
         document.getElementById("allData").textContent = error;
     }
 }
+
+
+const pokemonListButton = document.getElementById("pokemonListButton");
+const listOfPokemon = document.getElementById("listOfPokemon");
+
+pokemonListButton.addEventListener("click", event => {
+    if(listOfPokemon.style.display === "none") 
+        listOfPokemon.style.display = "block";
+    else listOfPokemon.style.display = "none";
+})
+
+let arrayNames = [];
+async function fetchNames(){
+    try{
+        for(let i=1; i<=1025; i++)
+        {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+            if(!response.ok){
+                throw new Error("Could not fetch data");
+            }
+            const data = await response.json();
+            arrayNames.push(data.name);
+            const newListItem = document.createElement("h2");
+            newListItem.textContent = data.name;
+            document.getElementById("listOfPokemon").append(newListItem);
+            /*
+            theNames1 += "{";
+            theNames1 += `"name" : "${data.name}",`;
+            theNames1 += `"id" : "${i}"`;
+            theNames1 += "}";
+            if(i<151) theNames1 += ",";*/
+        }
+        //const namesData = JSON.parse(theNames1);
+        console.log(arrayNames);
+    }
+    catch(error){
+        console.log(error);
+        document.getElementById("allData").textContent = error;
+    }
+}
+fetchNames();
