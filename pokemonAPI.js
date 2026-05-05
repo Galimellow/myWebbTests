f0bbb89825c042e65b1e14cfd09daaf2e93287b5
Galimellow <alexpanes92@gmail.com>
2026-05-05 10:10:09 +0300

    Log Pokemon type ID in fetchData function
    Updated fetchData function to log Pokemon type ID.

--- pokemonAPI.js
+++ pokemonAPI.js
@@ -13,7 +13,7 @@ async function fetchData(){
     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
 
     try{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         
         if(!response.ok){
             throw new Error("Could not fetch data");
@@ -26,8 +26,9 @@ async function fetchData(){
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
         imgElement.style.width = "25%";
-        let x = JSON.stringify(data.name).replaceAll('"', '');
-        document.getElementById("allData").textContent = x;
+        
+        let pokemonId = JSON.stringify(data.types[0].type.name).replaceAll('"', '');
+        console.log(pokemonId);
     }
     catch(error){
         console.log(error);
